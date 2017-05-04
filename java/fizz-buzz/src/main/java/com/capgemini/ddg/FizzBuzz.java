c3300452ec3fbb95fc8027fbe6cdc7f3f872dfc7
Les Jones <les.jones@capgemini.com>
2017-05-04 14:41:49 +0100

    [LJ] Kata solution

--- java/fizz-buzz/src/main/java/com/capgemini/ddg/FizzBuzz.java
+++ java/fizz-buzz/src/main/java/com/capgemini/ddg/FizzBuzz.java
@@ -1,8 +1,28 @@
 package com.capgemini.ddg;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 public class FizzBuzz {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    static Collection<String> fizzBuzz(Collection<Integer> values) {
+        // technique for converting the collection on ints using Java 8 enhancements
+        return values.stream().
+                map(i -> fizzBuzz(i)).
+                collect(Collectors.toList());
     }
+
+    static String fizzBuzz(int value) {
+        if ((value % 15) == 0) {
+            return "fizzbuzz";
+        } else if ((value % 5) == 0) {
+            return "buzz";
+        } else if ((value % 3) == 0) {
+            return "fizz";
+        } else {
+            return "" + value;
+        }
+    }
+
 }
+
--- java/fizz-buzz/src/test/java/com/capgemini/ddg/FizzBuzzTest.java
+++ java/fizz-buzz/src/test/java/com/capgemini/ddg/FizzBuzzTest.java
@@ -3,13 +3,50 @@ package com.capgemini.ddg;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
  * Unit test for FizzBuzz application.
  */
 public class FizzBuzzTest {
 
     @Test
-    public void testSomething() {
-        Assert.fail("Implement Me!");
+    public void testFizzValues() {
+        Assert.assertEquals("fizz", FizzBuzz.fizzBuzz(3));
+        Assert.assertEquals("fizz", FizzBuzz.fizzBuzz(6));
+        Assert.assertEquals("fizz", FizzBuzz.fizzBuzz(27));
     }
+
+    @Test
+    public void testBuzzValues() {
+        Assert.assertEquals("buzz", FizzBuzz.fizzBuzz(5));
+        Assert.assertEquals("buzz", FizzBuzz.fizzBuzz(10));
+    }
+
+    @Test
+    public void testFizzBuzzValues() {
+        Assert.assertEquals("fizzbuzz", FizzBuzz.fizzBuzz(15));
+        Assert.assertEquals("fizzbuzz", FizzBuzz.fizzBuzz(30));
+    }
+
+    @Test
+    public void testOtherValues() {
+        Assert.assertEquals("1", FizzBuzz.fizzBuzz(1));
+    }
+
+    @Test
+    public void testCollection1() {
+        Collection<Integer> values = Arrays.asList(1, 2, 3, 4, 5);
+        Collection<String> expected = Arrays.asList("1", "2", "fizz", "4", "buzz");
+        Assert.assertEquals(expected, FizzBuzz.fizzBuzz(values));
+    }
+
+    @Test
+    public void testCollection2() {
+        Collection<Integer> values = Arrays.asList(1, 3, 5, 8, 9, 10, 12, 13, 15, 20);
+        Collection<String> expected = Arrays.asList("1", "fizz", "buzz", "8", "fizz", "buzz", "fizz", "13", "fizzbuzz", "buzz");
+        Assert.assertEquals(expected, FizzBuzz.fizzBuzz(values));
+    }
+
 }
