9771256e971504eb6d24f893c139a3b7056924aa
Forster <david.a.forster@capgemini.com>
2017-05-11 13:10:00 +0100

    Dave's Fib Kata

--- java/starters/fibonacci-generator/src/main/java/com/capgemini/ddg/FibonacciGenerator.java
+++ java/starters/fibonacci-generator/src/main/java/com/capgemini/ddg/FibonacciGenerator.java
@@ -1,6 +1,23 @@
 package com.capgemini.ddg;
 
-public class FibonacciGenerator {
-
-
+public class FibonacciGenerator 
+{
+	static int fibonacciNumber = 0;
+	
+	public static int generate(int input)
+	{		
+//		if (input == 6)
+//			return input + 2;
+//		else if (input <= 1 || input >= 5)
+//			return input;
+//		else
+//			return input - 1;
+		
+		if (input == 0)
+			return 0;
+		else if (input == 1)
+			return 1;
+		else
+			return generate(input-1) + generate(input-2);
+	}
 }
--- java/starters/fibonacci-generator/src/test/java/com/capgemini/ddg/FibonacciGeneratorTest.java
+++ java/starters/fibonacci-generator/src/test/java/com/capgemini/ddg/FibonacciGeneratorTest.java
@@ -6,10 +6,56 @@ import org.junit.Test;
 /**
  * Test Class for @{@link FibonacciGenerator}
  */
-public class FibonacciGeneratorTest {
-
+public class FibonacciGeneratorTest 
+{
     @Test
-    public void testMe() {
-        Assert.fail("Implement Me!");
+    public void firstTest() 
+    {
+    	Assert.assertEquals(0, FibonacciGenerator.generate(0));
+    }
+    
+    @Test
+    public void secondTest()
+    {
+    	Assert.assertEquals(1, FibonacciGenerator.generate(1));
+    }
+    
+    @Test
+    public void thirdTest()
+    {
+    	Assert.assertEquals(1, FibonacciGenerator.generate(2));
+    }
+    
+    @Test
+    public void fourthTest()
+    {
+    	Assert.assertEquals(2, FibonacciGenerator.generate(3));
+    }
+    
+    @Test
+    public void fifthTest()
+    {
+    	Assert.assertEquals(3,  FibonacciGenerator.generate(4));
+    }
+    
+    @Test
+    public void sixthTest()
+    {
+    	Assert.assertEquals(5, FibonacciGenerator.generate(5));
+    }
+    
+    @Test
+    public void seventhTest()
+    {
+    	Assert.assertEquals(8, FibonacciGenerator.generate(6));
+    }
+    
+    @Test
+    public void eigthTest()
+    {
+    	Assert.assertEquals(13, FibonacciGenerator.generate(7));
+    	Assert.assertEquals(21, FibonacciGenerator.generate(8));
+    	Assert.assertEquals(34, FibonacciGenerator.generate(9));
+    	Assert.assertEquals(55, FibonacciGenerator.generate(10));
     }
 }
