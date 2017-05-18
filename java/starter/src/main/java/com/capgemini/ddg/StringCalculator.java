47b7b96508a457633d58b0c93b494a3e5e0f6f08
Les Jones <les.jones@capgemini.com>
2017-05-18 12:26:32 +0100

    Implementation using Java 8 streams

--- java/starter/src/main/java/com/capgemini/ddg/StringCalculator.java
+++ java/starter/src/main/java/com/capgemini/ddg/StringCalculator.java
@@ -1,12 +1,17 @@
 package com.capgemini.ddg;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class StringCalculator {
 
     static int sum(String numbers) {
         if( numbers.isEmpty() ) {
             return 0;
         } else {
-            return Integer.parseInt(numbers);
+            Stream<String> sNums = Arrays.stream(numbers.split(","));
+            Stream<Integer> iNums = sNums.map( i -> Integer.parseInt(i));
+            return iNums.reduce(0, (i1, i2) -> i1 + i2);
         }
     }
 }
