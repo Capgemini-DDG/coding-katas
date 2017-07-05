188b77783137d2db3bf714964f5b7906de70b20c
david-forster10 <dforster10@hotmail.com>
2017-07-05 16:02:36 +0100

    Hopefully repairing my projects as were unable to open them when clone from repo

---
+++ java/solutions/Numbers to Words/src/main/SolutionTest.java
@@ -0,0 +1,309 @@
+package main;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+	@Test
+	public void Test1() {
+		Solution solution = new Solution();
+		assertEquals("three", solution.Main(3));
+	}
+
+	@Test
+	public void Test3() {
+		Solution solution = new Solution();
+		assertEquals("six", solution.Main(6));
+	}
+
+	@Test
+	public void Test4() {
+		Solution solution = new Solution();
+		assertEquals("nine", solution.Main(9));
+	}
+
+	@Test
+	public void Test2() {
+		Solution solution = new Solution();
+		assertEquals("three hundred", solution.Main(300));
+	}
+
+	@Test
+	public void Test5() {
+		Solution solution = new Solution();
+		assertEquals("seven thousand", solution.Main(7000));
+	}
+
+	@Test
+	public void Test6() {
+		Solution solution = new Solution();
+		assertEquals("five hundred", solution.Main(500));
+	}
+
+	@Test
+	public void Test7() {
+		Solution solution = new Solution();
+		assertEquals("nine hundred", solution.Main(900));
+	}
+
+	@Test
+	public void Test8() {
+		Solution solution = new Solution();
+		assertEquals("sixty six", solution.Main(66));
+	}
+
+	@Test
+	public void Test9() {
+		Solution solution = new Solution();
+		assertEquals("fourty three", solution.Main(43));
+	}
+
+	@Test
+	public void Test10() {
+		Solution solution = new Solution();
+		assertEquals("ninety five", solution.Main(95));
+	}
+
+	@Test
+	public void Test11() {
+		Solution solution = new Solution();
+		assertEquals("fifteen", solution.Main(15));
+	}
+
+	@Test
+	public void Test12() {
+		Solution solution = new Solution();
+		assertEquals("ten", solution.Main(10));
+	}
+
+	@Test
+	public void Test13() {
+		Solution solution = new Solution();
+		assertEquals("seventeen", solution.Main(17));
+	}
+
+	@Test
+	public void Test14() {
+		Solution solution = new Solution();
+		assertEquals("thirteen", solution.Main(13));
+	}
+
+	@Test
+	public void Test15() {
+		Solution solution = new Solution();
+		assertEquals("five", solution.Main(5));
+	}
+
+	@Test
+	public void Test16() {
+		Solution solution = new Solution();
+		assertEquals("fourteen", solution.Main(14));
+	}
+
+	@Test
+	public void Test17() {
+		Solution solution = new Solution();
+		assertEquals("twelve", solution.Main(12));
+	}
+
+	@Test
+	public void Test18() {
+		Solution solution = new Solution();
+		assertEquals("eleven", solution.Main(11));
+	}
+
+	@Test
+	public void Test19() {
+		Solution solution = new Solution();
+		assertEquals("one", solution.Main(1));
+	}
+
+	@Test
+	public void Test20() {
+		Solution solution = new Solution();
+		assertEquals("one hundred and fifty four", solution.Main(154));
+	}
+
+	@Test
+	public void Test21() {
+		Solution solution = new Solution();
+		assertEquals("one hundred and thirty two", solution.Main(132));
+	}
+
+	@Test
+	public void Test22() {
+		Solution solution = new Solution();
+		assertEquals("one hundred and fifty two", solution.Main(152));
+	}
+
+	@Test
+	public void Test23() {
+		Solution solution = new Solution();
+		assertEquals("twenty six thousand one hundred and ninety five", solution.Main(26195));
+	}
+
+	@Test
+	public void Test24() {
+		Solution solution = new Solution();
+		assertEquals("two thousand four hundred and sixty two", solution.Main(2462));
+	}
+
+	@Test
+	public void Test25() {
+		Solution solution = new Solution();
+		assertEquals("five thousand and ninety seven", solution.Main(5097));
+	}
+
+	@Test
+	public void Test26() {
+		Solution solution = new Solution();
+		assertEquals("fifty thousand nine hundred and seventy", solution.Main(50970));
+	}
+
+	@Test
+	public void Test27() {
+		Solution solution = new Solution();
+		assertEquals("eight hundred and fourty three thousand five hundred and two", solution.Main(843502));
+	}
+
+	@Test
+	public void Test28() {
+		Solution solution = new Solution();
+		assertEquals("three hundred and six thousand nine hundred and thirty eight", solution.Main(306938));
+	}
+
+	@Test
+	public void Test29() {
+		Solution solution = new Solution();
+		assertEquals("twenty four thousand three hundred and eighty two", solution.Main(24382));
+	}
+
+	@Test
+	public void Test30() {
+		Solution solution = new Solution();
+		assertEquals("fifty thousand eight hundred and seventy", solution.Main(50870));
+	}
+
+	@Test
+	public void Test31() {
+		Solution solution = new Solution();
+		assertEquals("fifty", solution.Main(50));
+	}
+
+	@Test
+	public void Test32() {
+		Solution solution = new Solution();
+		assertEquals("three hundred and twenty", solution.Main(320));
+	}
+
+	@Test
+	public void Test33() {
+		Solution solution = new Solution();
+		assertEquals("one hundred and two", solution.Main(102));
+	}
+
+	@Test
+	public void Test34() {
+		Solution solution = new Solution();
+		assertEquals("two hundred and eighty three thousand nine hundred and fourty", solution.Main(283940));
+	}
+
+	@Test
+	public void Test35() {
+		Solution solution = new Solution();
+		assertEquals("one hundred thousand three hundred and twenty three", solution.Main(100323));
+	}
+
+	@Test
+	public void Test36() {
+		Solution solution = new Solution();
+		assertEquals("three hundred and thirty three thousand three hundred and thirty three", solution.Main(333333));
+	}
+
+	@Test
+	public void Test37() {
+		Solution solution = new Solution();
+		assertEquals("four hundred and nine thousand eight hundred and twenty seven", solution.Main(409827));
+	}
+
+	@Test
+	public void Test38() {
+		Solution solution = new Solution();
+		assertEquals("eighty seven thousand three hundred and ninety six", solution.Main(87396));
+	}
+
+	@Test
+	public void Test39() {
+		Solution solution = new Solution();
+		assertEquals("nine hundred and thirty eight thousand seven hundred and eighty four", solution.Main(938784));
+	}
+
+	@Test
+	public void Test40() {
+		Solution solution = new Solution();
+		assertEquals("five hundred and twenty three thousand four hundred and sixty nine", solution.Main(523469));
+	}
+
+	@Test
+	public void Test41() {
+		Solution solution = new Solution();
+		assertEquals("four hundred and ninety two thousand eight hundred and thirty seven", solution.Main(492837));
+	}
+
+	@Test
+	public void Test42() {
+		Solution solution = new Solution();
+		assertEquals("six hundred and fifty two thousand nine hundred and fifteen", solution.Main(652915));
+	}
+
+	@Test
+	public void Test43() {
+		Solution solution = new Solution();
+		assertEquals("one hundred and fifteen", solution.Main(115));
+	}
+
+	@Test
+	public void Test44() {
+		Solution solution = new Solution();
+		assertEquals("three hundred and eighty two thousand nine hundred and twelve", solution.Main(382912));
+	}
+
+	@Test
+	public void Test45() {
+		Solution solution = new Solution();
+		assertEquals("four hundred and eighty three thousand seven hundred and eleven", solution.Main(483711));
+	}
+
+	@Test
+	public void Test46() {
+		Solution solution = new Solution();
+		assertEquals("three hundred and ninety two thousand eight hundred and fifty four", solution.Main(392854));
+	}
+
+	@Test
+	public void Test47() {
+		Solution solution = new Solution();
+		assertEquals("nine hundred and thirty seven thousand one hundred and eighty three", solution.Main(937183));
+	}
+
+	@Test
+	public void Test48() {
+		Solution solution = new Solution();
+		assertEquals("eight hundred and one thousand nine hundred and eleven", solution.Main(801911));
+	}
+
+	@Test
+	public void Test49() {
+		Solution solution = new Solution();
+		assertEquals("four hundred and eighty two thousand nine hundred and thirty seven", solution.Main(482937));
+	}
+
+	@Test
+	public void Test50() {
+		Solution solution = new Solution();
+		assertEquals("one hundred and fifty four", solution.Main(154));
+	}
+	
+}
